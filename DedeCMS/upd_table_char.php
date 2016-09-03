e76d657f6a721e27e1dffb875c69e9efef1d185c
benhuang <benhuang1024@163.com>
2016-09-03 17:32:03 +0800

    add keywords loop
    增加关键词循环

--- DedeCMS/upd_table_char.php
+++ DedeCMS/upd_table_char.php
@@ -3,25 +3,26 @@ $dbms='mysql';     //数据库类型
 $host='localhost'; //数据库主机名
 $user='root';      //数据库连接用户名
 $pass='blog.benhuang1024.com';          
-$rpfield = 'body'; // update table char	
-$rpstring = 'ben';  // update string
-$tostring = '';
+$rpfield = 'body'; // update table char		
+$tostring = ''; //	
 $sql_arr = array(table1,table2);
-foreach($sql_arr as $sql_one){
-	$mysqli = new MySQLi($host,$user,$pass,$sql_one);	
-	$mysqli->query("set names UTF8");										
-	$exptable_obj = $mysqli->query("show tables like '%_addonarticle'");	// 获取表			 			
-	 $exptable = mysqli_fetch_array($exptable_obj);
-	$exptable = $exptable[0];			
-	$rs = $mysqli->query("UPDATE $exptable SET $rpfield=REPLACE($rpfield,'$rpstring','$tostring')");
-	$mysqli->query("OPTIMIZE TABLE `$exptable`");
-		echo "UPDATE $exptable SET $rpfield=REPLACE($rpfield,'$rpstring','$tostring')"."<br />";
-	if($rs){
-		echo "成功完成数据替换！".$sql_one."<br />";		
-	}else{
-		echo "数据替换失败！".$sql_one."<br />";		
+$rpstring_arr = array('违规词1','违规词2');
+foreach($rpstring_arr as $rpstring_one){	
+$rpstring = $rpstring_one;
+	foreach($sql_arr as $sql_one){
+			$mysqli = new MySQLi($host,$user,$pass,$sql_one);	
+			$mysqli->query("set names UTF8");										
+			$exptable_obj = $mysqli->query("show tables like '%_addonarticle'");	// 获取表			 			
+			 $exptable = mysqli_fetch_array($exptable_obj);
+			$exptable = $exptable[0];			
+			$rs = $mysqli->query("UPDATE $exptable SET $rpfield=REPLACE($rpfield,'$rpstring','$tostring')");
+			$mysqli->query("OPTIMIZE TABLE `$exptable`");					
+			if($rs){
+				echo $sql_one."成功完成&nbsp;".$rpstring."&nbsp;数据替换!<br />";		
+			}else{
+				echo $sql_one."中数据&nbsp;".$rpstring."&nbsp;替换失败！<br />";		
+			}
+			$mysqli->close();				
 	}
-	$mysqli->close();	
 }
-
 
